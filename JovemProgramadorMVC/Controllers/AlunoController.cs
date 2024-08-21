bab54fcd6839ed6201436cbba81364a6400cfa5b
beagroh <beagrohh@gmail.com>
2024-08-20 21:27:13 -0300

    ascrescentar

--- JovemProgramadorMVC/Controllers/AlunoController.cs
+++ JovemProgramadorMVC/Controllers/AlunoController.cs
@@ -4,8 +4,14 @@ namespace JovemProgramadorMVC.Controllers
 {
     public class AlunoController : Controller
     {
+        private readonly IAlunoRepositorio _alunoRepositorio;
+        public AlunoController(IAlunoRepositorio alunoRepositorio)
+        {
+                _alunoRepositorio = alunoRepositorio;
+        }
         public IActionResult Index()
         {
+            var aluno = _alunoRepositorio.BuscarAlunos();
             return View();
         }
     }
